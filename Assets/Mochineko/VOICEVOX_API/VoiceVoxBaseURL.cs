b2c9fa7abc79e5b10a9da29f6fabd4f2b6111ad4
mochi-neko <t.o.e.4315@gmail.com>
2023-04-12 14:32:25 +0900

    Chage base URL to direct IP

--- Assets/Mochineko/VOICEVOX_API/VoiceVoxBaseURL.cs
+++ Assets/Mochineko/VOICEVOX_API/VoiceVoxBaseURL.cs
@@ -6,7 +6,7 @@ namespace Mochineko.VOICEVOX_API
     /// </summary>
     public static class VoiceVoxBaseURL
     {
-        private const string DefaultBaseURL = "http://localhost:50021";
+        private const string DefaultBaseURL = "http://127.0.0.1:50021";
         public static string BaseURL { get; set; } = DefaultBaseURL;
     }
 }
